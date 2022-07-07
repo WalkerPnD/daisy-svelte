9fc689bed0ed61942aa77f31359b4d69278140dd
Koji Carvalho <stylethewalker@gmail.com>
2022-07-06 22:48:05 -0300

    feat(breadcrumbs)

---
+++ src/lib/navigation/breadcrumbs/Breadcrumbs.stories.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import Breadcrumbs from './Breadcrumbs.svelte';
+</script>
+
+<!-- 👇 Creates specific parameters for the story -->
+<Meta
+	title="Navigation/Breadcrumbs"
+	component={Breadcrumbs}
+	argTypes={{
+		label: { control: 'text' },
+		textColor: { control: 'color' },
+	}}
+/>
+
+<Template let:args>
+	<Breadcrumbs {...args}>
+		<slot />
+	</Breadcrumbs>
+</Template>
+
+<Story name="Breadcrumbs with title only">
+	<Breadcrumbs class="bg-base-100">
+		<li><a>Home</a></li>
+		<li><a>Documents</a></li>
+		<li>Add Document</li>
+	</Breadcrumbs>
+</Story>
---
+++ src/lib/navigation/breadcrumbs/Breadcrumbs.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { bindClasses } from '../../utils/bind-classes';
+	const prefix = 'breadcrumbs';
+
+	let className = '';
+	export { className as class };
+	// export let itens = [];
+
+	$: classes = bindClasses([prefix, className]);
+</script>
+
+<div {...$$restProps} class={classes}>
+	<ul>
+		<slot />
+	</ul>
+	<!-- {#if Array.isArray(itens) && itens.length > 0}
+		<ul>
+			{#each itens as item}
+				<li class="relative bg-light simple-list">
+					<slot {item} name="item" />
+				</li>
+			{/each}
+		</ul>
+	{/if} -->
+</div>
