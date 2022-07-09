c4f912346639eac1a2dcfbf4fbabedba20c0e080
Koji Carvalho <stylethewalker@gmail.com>
2022-07-09 09:43:04 -0300

    fix export bug

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,11 @@
 // actions
-export * as Button from './actions/button/Button.svelte';
-export * as Dropdown from './actions/dropdown/Dropdown.svelte';
-export * as Modal from './actions/modal/Modal.svelte';
+import Button from './actions/button/Button.svelte';
+import Dropdown from './actions/dropdown/Dropdown.svelte';
+import Modal from './actions/modal/Modal.svelte';
 
 // navigation
-export * as Breadcrumbs from './navigation/breadcrumbs/Breadcrumbs.svelte';
-export * as Navbar from './navigation/navbar/Navbar.svelte';
-export * as Link from './navigation/link/Link.svelte';
+import Breadcrumbs from './navigation/breadcrumbs/Breadcrumbs.svelte';
+import Navbar from './navigation/navbar/Navbar.svelte';
+import Link from './navigation/link/Link.svelte';
+
+export { Button, Dropdown, Modal, Breadcrumbs, Navbar, Link };
