bcd11da0387ec532538066fd6eea4999d6031566
Koji Carvalho <stylethewalker@gmail.com>
2022-07-23 10:54:54 -0300

    fix(export): Hero

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,9 +6,12 @@ import Modal from './actions/modal/Modal.svelte';
 // Data Display
 import Avatar from './data-display/avatar/Avatar.svelte';
 
+// Layout
+import Hero from './layout/hero/Hero.svelte';
+
 // navigation
 import Breadcrumbs from './navigation/breadcrumbs/Breadcrumbs.svelte';
 import Navbar from './navigation/navbar/Navbar.svelte';
 import Link from './navigation/link/Link.svelte';
 
-export { Button, Dropdown, Modal, Breadcrumbs, Navbar, Link, Avatar };
+export { Button, Dropdown, Modal, Breadcrumbs, Navbar, Link, Avatar, Hero };
